1954abdda186c3dad44878b2502b3f241c25d54d
Robert Johansson <robert.johansson@gmail.com>
2025-01-08 09:54:46 +0100

    Update test_ona.py

--- test_ona.py
+++ test_ona.py
@@ -1,30 +1,27 @@
 import unittest
-from ona import Truth_Deduction
+from ona import truth_deduction, truth_w2c, truth_induction, truth_intersection
 
-# FILE: test_ona.py
+class TestONAFunctions(unittest.TestCase):
 
+    def test_truth_deduction(self):
+        self.assertEqual(truth_deduction((1, 0.5), (2, 0.8)), (4, 1.6))
+        self.assertEqual(truth_deduction((2, 0.3), (3, 0.7)), (9, 1.89))
+        self.assertEqual(truth_deduction((0, 1.0), (4, 0.6)), (16, 9.6))
 
-class TestTruthDeduction(unittest.TestCase):
-    
-    def test_case_1(self):
-        v1 = (2, 3)
-        v2 = (4, 5)
-        result = Truth_Deduction(v1, v2)
-        self.assertEqual(result, (16, 240))
-    
-    def test_case_2(self):
-        v1 = (1, 2)
-        v2 = (3, 4)
-        result = Truth_Deduction(v1, v2)
-        self.assertEqual(result, (9, 72))
-    
-    def test_case_3(self):
-        v1 = (0, 1)
-        v2 = (2, 3)
-        result = Truth_Deduction(v1, v2)
-        self.assertEqual(result, (4, 12))
+    def test_truth_w2c(self):
+        self.assertEqual(truth_w2c(2), 2 / (2 + 1.0))
+        self.assertEqual(truth_w2c(5), 5 / (5 + 1.0))
+        self.assertEqual(truth_w2c(0), 0 / (0 + 1.0))
 
+    # def test_truth_induction(self):
+    #     self.assertEqual(truth_induction((1, 0.5), (2, 0.8)), (2, truth_w2c(1 * 0.5 * 0.8)))
+    #     self.assertEqual(truth_induction((2, 0.3), (3, 0.7)), (3, truth_w2c(2 * 0.3 * 0.7)))
+    #     self.assertEqual(truth_induction((0, 1.0), (4, 0.6)), (4, truth_w2c(0 * 1.0 * 0.6)))
 
+    def test_truth_intersection(self):
+        self.assertEqual(truth_intersection((1, 0.5), (2, 0.8)), (2, 0.4))
+        self.assertEqual(truth_intersection((2, 0.3), (3, 0.7)), (6, 0.21))
+        self.assertEqual(truth_intersection((0, 1.0), (4, 0.6)), (0, 0.6))
 
 if __name__ == '__main__':
     unittest.main()
